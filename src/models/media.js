1fd26f2887b28429fd4e9d2871508516dd6749fd
Raphael Didjurgeit <raphael.didjurgeit@joblocal.de>
2023-07-31 13:46:59 +0200

    [DR-17962-add-resized-media-versions] add resized media urls

--- src/models/media.js
+++ src/models/media.js
@@ -9,6 +9,8 @@ export default {
     type: null,
     url: null,
     thumbnail_url: null,
+    logo_profile_url: null,
+    logo_list_url: null,
     video_source: null,
     created_at: null,
     updated_at: null,
